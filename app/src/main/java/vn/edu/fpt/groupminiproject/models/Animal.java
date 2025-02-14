218a9ea5bc4d0103838a40e9e714d96affb69c63
neru <ngocngocthuc@gmail.com>
2025-02-14 21:41:16 +0700

    add new files

--- app/src/main/java/vn/edu/fpt/groupminiproject/models/Animal.java
+++ app/src/main/java/vn/edu/fpt/groupminiproject/models/Animal.java
@@ -4,8 +4,31 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 
 public class Animal {
+    private String displayName;
     private SeekBar seekBar;
     private ImageView badge;
-    private String displayName;
 
+    public Animal(String displayName, SeekBar seekBar, ImageView badge) {
+        this.displayName = displayName;
+        this.seekBar = seekBar;
+        this.badge = badge;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public SeekBar getSeekBar() {
+        return seekBar;
+    }
+
+    public ImageView getBadge() {
+        return badge;
+    }
+
+    public int getRandomSpeed() {
+        // Implement logic to generate a random speed for the animal from 1 - 3
+        int speed = (int) (Math.random() * 3) + 1;
+        return Math.min(speed, 3);
+    }
 }
