a15422c189c08cd288724bcc8a23d6843761efa2
neru <ngocngocthuc@gmail.com>
2025-02-14 21:14:25 +0700

    login layout

---
+++ app/src/main/java/vn/edu/fpt/groupminiproject/models/Animal.java
@@ -0,0 +1,11 @@
+package vn.edu.fpt.groupminiproject.models;
+
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class Animal {
+    private SeekBar seekBar;
+    private ImageView badge;
+    private String displayName;
+
+}
